2ffa52cd502384f6cd147793cb4e14d1284f93d1
Adrian Bean <adrian@beanit.co.uk>
2017-10-31 20:47:12 +0000

    hopefully the last commit to fix all the things

--- application/views/about/index.php
+++ application/views/about/index.php
@@ -1,3 +1,9 @@
 <div id="infoMessage"><?php echo $message;?></div>
 
 <?php echo file_get_contents('http://loripsum.net/api/5'); ?>
+<?php
+
+/*
+    just a random text change to allow for a commit
+*/
+?>
